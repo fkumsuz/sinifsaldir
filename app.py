623198a073d5a5a6d22c5d0173942e1738ba735a
fkumsuz <kumsuzfurkan@gmail.com>
2024-04-22 16:56:28 +0300

    app yeni foldera göre degistirildi

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ import os
 app = Flask(__name__)
 
 # Folder containing the images
-image_folder = "static/images"
+image_folder = "/images"
 # Get the list of image files and names in the folder
 image_files = [os.path.join(image_folder, file) for file in os.listdir(image_folder) if file.endswith((".jpg", ".jpeg", ".png"))]
 image_names = [os.path.splitext(os.path.basename(file))[0] for file in image_files]
